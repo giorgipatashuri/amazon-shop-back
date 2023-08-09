ae66ef229871a5184210ff7bf6c4c90fc6ee87cc
giorgipatashuri <patashuri2003@gmail.com>
2023-08-09 21:28:03 +0400

    added prisma service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
   app.enableCors()
   app.setGlobalPrefix("api")
   await app.listen(4800);
