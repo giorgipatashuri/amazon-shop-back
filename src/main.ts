b3c9b418c318fb8563b6a249966b1833a7f77dd7
giorgipatashuri <patashuri2003@gmail.com>
2023-08-10 21:42:25 +0400

    added pipe for global validation and change port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors()
   app.setGlobalPrefix("api")
-  await app.listen(4800);
+  await app.listen(8888);
 }
 bootstrap(); 
