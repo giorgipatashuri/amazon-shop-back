f061563eee7d18e00b768d4874d741a55c043fb2
giorgipatashuri <patashuri2003@gmail.com>
2023-08-22 21:07:29 +0400

    added user endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Put } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
 
+  @Auth()
+  @Get("profile")
+  getProfile(@CurrentUser("id") id:number){
+    return this.userService.getById(id)
+  }
+
+  @Auth()
+  @Put("profile")
+  updateProfile(@CurrentUser("id") id:number, @Body() dto:UpdateUserDto){
+    return this.userService.updateProfile(id,dto)
+  }
+
+  @Auth()
+  @Patch("profile/favorites/:productId")
+  toggleFavorites(@CurrentUser("id") id:number, @Param("productId") productId:string){
+    return this.userService.toggleFavorite(id,+productId)
+  }
 }
