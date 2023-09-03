48c6dc2d0a2c988f8a242472e8c9735ee026769a
giorgipatashuri <patashuri2003@gmail.com>
2023-09-03 12:51:40 +0400

    fixed wake-up url

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
+
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
-  app.setGlobalPrefix("api")
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  app.use('/live', (_req, res) => {
+    res.json({ status: true });
+  });
+
   await app.listen(8888);
 }
-bootstrap(); 
+bootstrap();
